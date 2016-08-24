07ddba446bf1cc085e88855dd7036f863058b6c6
dlinch <dlinch33@gmail.com>
2016-08-23 22:35:59 -0600

    added the printing of personalized letters to all the players

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,30 +1,23 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+// Assigning player info to variables.
 let players: [String] = ["Joe Smith", "Jill Tanner", "Bill Bon", "Eva Gordon", "Matt Gill", "Kimmy Stein", "Sammy Adams", "Karl Saygan", "Suzane Greenberg", "Sal Dali", "Joe Kavalier", "Ben Finkelstein", "Diego Soto", "Chloe Alaska", "Arnold Willis", "Philip Helm", "Les Clay", "Herschel Krustofski"]
+
+var playerInfo: [String: [String]] = ["Joe Smith": ["42", "true", "Jim and Jan Smith"], "Jill Tanner": ["36", "true", "Clara Tanner"], "Bill Bon": ["43", "true", "Sara and Jenny Bon"], "Eva Gordon": ["45", "false", "Wendy and Mike Gordon"], "Matt Gill": ["40", "false", "Charles and Sylvia Gill"], "Kimmy Stein": ["41", "false", "Bill and Hillary Stein"], "Sammy Adams": ["45", "false", "Jeff Adams"], "Karl Saygan": ["42", "true", "Heather Bledsoe"], "Suzane Greenberg": ["44", "true", "Henrietta Dumas"], "Sal Dali": ["41", "false", "Gala Dali"], "Joe Kavalier": ["41", "false", "Sam and Elaine Kavalier"], "Ben Finkelstein": ["44", "false", "Aaron and Jill Finkelstein"], "Diego Soto": ["41", "true", "Robin and Sarika Soto"], "Chloe Alaska": ["47", "false", "David and Jamie Alaska"], "Arnold Willis": ["43", "false", "Claire Willis"], "Philip Helm": ["44", "true", "Thomas Helm and Eva Jones"], "Les Clay": ["42", "true", "Wynonna Brown"], "Herschel Krustofski": ["45", "true", "Hyman and Rachel Krustofski"]]
+// Useful variables, including the creation of the team data collections, as well numbers to help do math against in future code
 let playerCount = players.count
-let playerInfo: [String: [String]] = ["Joe Smith": ["42", "true", "Jim and Jan Smith"], "Jill Tanner": ["36", "true", "Clara Tanner"], "Bill Bon": ["43", "true", "Sara and Jenny Bon"], "Eva Gordon": ["45", "false", "Wendy and Mike Gordon"], "Matt Gill": ["40", "false", "Charles and Sylvia Gill"], "Kimmy Stein": ["41", "false", "Bill and Hillary Stein"], "Sammy Adams": ["45", "false", "Jeff Adams"], "Karl Saygan": ["42", "true", "Heather Bledsoe"], "Suzane Greenberg": ["44", "true", "Henrietta Dumas"], "Sal Dali": ["41", "false", "Gala Dali"], "Joe Kavalier": ["41", "false", "Sam and Elaine Kavalier"], "Ben Finkelstein": ["44", "false", "Aaron and Jill Finkelstein"], "Diego Soto": ["41", "true", "Robin and Sarika Soto"], "Chloe Alaska": ["47", "false", "David and Jamie Alaska"], "Arnold Willis": ["43", "false", "Claire Willis"], "Philip Helm": ["44", "true", "Thomas Helm and Eva Jones"], "Les Clay": ["42", "true", "Wynonna Brown"], "Herschel Krustofski": ["45", "true", "Hyman and Rachel Krustofski"]]
 let teamCount: Int = 3
 let playersPerTeam: Int = players.count/teamCount
 var Sharks: [String] = []
 var Dragons: [String] = []
 var Raptors: [String] = []
-func switchActiveTeam(team: [String])-> Void {
-    print("I was here")
-    if team == Sharks {
-        lastAppended = Dragons
-    } else if team == Dragons {
-        lastAppended = Raptors
-    } else {
-        lastAppended = Sharks
-    }
-    
-}
-var lastAppended: [String] = Sharks
 var experiencedPlayerCount: Int = 0
 var inexperiencedPlayerCount: Int = 0
 
-func sorty() -> Void {
+// Function to help figure out how many children of each experience level there are in total
+func experienceAnalysis() -> Void {
     var index: Int = 0
     while index < playerCount {
         if playerInfo[players[index]]![1] == "true" {
@@ -36,8 +29,9 @@ func sorty() -> Void {
     }
 }
 
+// Function to sort the students.
 func sort() -> Void {
-    sorty()
+    experienceAnalysis()
     var index: Int = 0
     var experiencedRaptors: Int = 0
     var inexperiencedRaptors: Int = 0
@@ -48,26 +42,27 @@ func sort() -> Void {
 
     while index < playerCount {
         if Sharks.count < playersPerTeam && experiencedSharks < experiencedPlayerCount/teamCount {
-            print(experiencedSharks)
             if playerInfo[players[index]]![1] == "true" {
                 Sharks.append(players[index])
-                 experiencedSharks+=1
+                playerInfo[players[index]]!.append("Sharks")
+                experiencedSharks+=1
                 index+=1
                 continue
             }
         }
         if Sharks.count < playersPerTeam && inexperiencedSharks < inexperiencedPlayerCount/teamCount {
-            print(inexperiencedSharks)
             if playerInfo[players[index]]![1] == "false" {
             Sharks.append(players[index])
+            playerInfo[players[index]]!.append("Sharks")
             inexperiencedSharks+=1
-                index+=1
+            index+=1
             continue
             }
         }
         if Raptors.count < playersPerTeam && experiencedRaptors < experiencedPlayerCount/teamCount {
             if playerInfo[players[index]]![1] == "true" {
                 Raptors.append(players[index])
+                playerInfo[players[index]]!.append("Raptors")
                 experiencedRaptors+=1
                 index+=1
                 continue
@@ -76,6 +71,7 @@ func sort() -> Void {
         if Raptors.count < playersPerTeam && inexperiencedRaptors < inexperiencedPlayerCount/teamCount {
             if playerInfo[players[index]]![1] == "false" {
                 Raptors.append(players[index])
+                playerInfo[players[index]]!.append("Raptors")
                 inexperiencedRaptors+=1
                 index+=1
                 continue
@@ -84,6 +80,7 @@ func sort() -> Void {
         if Dragons.count < playersPerTeam && experiencedDragons < experiencedPlayerCount/teamCount {
             if playerInfo[players[index]]![1] == "true" {
                 Dragons.append(players[index])
+                playerInfo[players[index]]!.append("Dragons")
                 experiencedDragons+=1
                 index+=1
                 continue
@@ -92,6 +89,7 @@ func sort() -> Void {
         if Dragons.count < playersPerTeam && inexperiencedDragons < inexperiencedPlayerCount/teamCount {
             if playerInfo[players[index]]![1] == "false" {
                 Dragons.append(players[index])
+                playerInfo[players[index]]!.append("Dragons")
                 inexperiencedDragons+=1
                 index+=1
                 continue
@@ -101,12 +99,25 @@ func sort() -> Void {
     }
 }
 
+// Function to print the personalized letters to all the parents
 func letterGenerator() -> Void {
-    
+    var index: Int = 0
+    while index < playerCount {
+        let letter: String = "Hello \(playerInfo[players[index]]![2]), I wanted you to know that your child \(players[index]) has officially been assigned to the \(playerInfo[players[index]]![3]). We're super excited, and wanted to let you know that their first practice is this Sunday at 7 p.m."
+        print(letter)
+    index+=1
+    }
 }
 
+// Function that turns the program
+func soccerScheduler() -> Void {
+    sort()
+    print(Sharks)
+    print(Dragons)
+    print(Raptors)
+    letterGenerator()
+}
 
-sort()
-print(Sharks)
-print(Dragons)
-print(Raptors)
+// Invocation of the program
+soccerScheduler()
+
